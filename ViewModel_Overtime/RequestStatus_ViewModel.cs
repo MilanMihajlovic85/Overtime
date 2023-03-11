0ca52e58bba16fa5c50451313414d32696fde5aa
Milan.Mihajlovic <Milan.Mihajlovic@HP-ELITEBOOK>
2023-03-11 10:50:06 +0100

    Added RequestData controller

---
+++ ViewModel_Overtime/RequestStatus_ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel_Overtime
+{
+   public class RequestStatus_ViewModel
+    {
+        public int Status { get; set; }
+        public string Description { get; set; }
+    }
+}
