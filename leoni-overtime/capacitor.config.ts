b3968d35884eb98ef28f1a3862250982e40d5f31
Ivan Nikolić- <guilleex@gmail.com>
2023-04-15 16:46:49 +0200

    Added IOS app

--- leoni-overtime/capacitor.config.ts
+++ leoni-overtime/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.leoni.overtime',
-  appName: 'Leoni - Overtime',
+  appName: 'Overtime',
   webDir: '../ClientApp',
   bundledWebRuntime: false
 };
